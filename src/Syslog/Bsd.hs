1b809830492d8e1ab588a469046666d3f114cde6
Brian McKeon <135748266+brianjosephmckeon@users.noreply.github.com>
2024-02-14 15:21:11 -0500

    Prepare 0.1.0.0 release.
    Reformatted.
    Use new .github workflows.
    Removed unused deps.
    Updated package metadata.

--- src/Syslog/Bsd.hs
+++ src/Syslog/Bsd.hs
@@ -1,42 +1,45 @@
-{-# language BangPatterns #-}
-{-# language DuplicateRecordFields #-}
-{-# language LambdaCase #-}
-{-# language NamedFieldPuns #-}
-{-# language TypeApplications #-}
-{-# language UnboxedTuples #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UnboxedTuples #-}
 
--- | Parse RFC 3164 messages. For example:
---
--- > <133>Feb 25 14:09:07 webserver syslogd: restart
--- > <0>Oct 22 10:52:01 scapegoat.dmz.example.org sched[0]: That's All Folks!
---
--- This library assumes that the @TAG@ field described by section 5.3 of
--- RFC 3164 is a process name. It also assumes that the optional bracketed
--- number that follows it is a process id. This library also addresses three
--- common extensions to RFC 3164:
---
--- * Some vendors include a year after the timestamp. For example:
---   @<14>Oct 15 11:14:59 2019 example.com ...@. When present, the year
---   is parsed and provided to the user.
--- * Some vendors include a priority that preceeds the process name. For
---   example: @<133>Aug 10 09:05:14 my-host notice tmsh[4726]: ...@. The
---   Linux man page for @syslog.conf@ lists these options for priority:
---   @debug@, @info@, @notice@, @warning@, @warn@, @err@, @error@, @crit@,
---   @alert@, @emerg@, @panic@. If a process name begins with any of these
---   keywords (followed by a space), the keyword and the trailing space
---   are removed from the process name, and the keyword is made available
---   in the @priority@ field.
--- * Cisco ASAs omit the hostname sometimes. This is totally bizarre and leads
---   to messages that looks like: @<190>Jun 08 2022 14:46:28: message@. In
---   this case, the hostname is set to the empty string.
+{- | Parse RFC 3164 messages. For example:
+
+> <133>Feb 25 14:09:07 webserver syslogd: restart
+> <0>Oct 22 10:52:01 scapegoat.dmz.example.org sched[0]: That's All Folks!
+
+This library assumes that the @TAG@ field described by section 5.3 of
+RFC 3164 is a process name. It also assumes that the optional bracketed
+number that follows it is a process id. This library also addresses three
+common extensions to RFC 3164:
+
+* Some vendors include a year after the timestamp. For example:
+  @<14>Oct 15 11:14:59 2019 example.com ...@. When present, the year
+  is parsed and provided to the user.
+* Some vendors include a priority that preceeds the process name. For
+  example: @<133>Aug 10 09:05:14 my-host notice tmsh[4726]: ...@. The
+  Linux man page for @syslog.conf@ lists these options for priority:
+  @debug@, @info@, @notice@, @warning@, @warn@, @err@, @error@, @crit@,
+  @alert@, @emerg@, @panic@. If a process name begins with any of these
+  keywords (followed by a space), the keyword and the trailing space
+  are removed from the process name, and the keyword is made available
+  in the @priority@ field.
+* Cisco ASAs omit the hostname sometimes. This is totally bizarre and leads
+  to messages that looks like: @<190>Jun 08 2022 14:46:28: message@. In
+  this case, the hostname is set to the empty string.
+-}
 module Syslog.Bsd
   ( -- * Types
-    Message(..)
-  , Process(..)
-  , Timestamp(..)
+    Message (..)
+  , Process (..)
+  , Timestamp (..)
+
     -- * Full Decode
   , decode
   , parser
+
     -- * Parsing Fragments
   , takePriority
   , takeTimestamp
@@ -47,17 +50,17 @@ module Syslog.Bsd
 import Prelude hiding (id)
 
 import Control.Monad (when)
-import Data.Bytes.Types (Bytes(Bytes))
 import Data.Bytes.Parser (Parser)
-import Data.Word (Word8,Word32)
+import Data.Bytes.Types (Bytes (Bytes))
+import Data.Word (Word32, Word8)
 
 import qualified Chronos
 import qualified Data.Bytes as Bytes
-import qualified Data.Maybe.Unpacked.Numeric.Word32 as Word32
 import qualified Data.Bytes.Parser as Parser
 import qualified Data.Bytes.Parser.Latin as Latin
 import qualified Data.Bytes.Parser.Unsafe as Unsafe
 import qualified Data.Bytes.Text.Latin1 as Latin1
+import qualified Data.Maybe.Unpacked.Numeric.Word32 as Word32
 
 data Message = Message
   { priority :: !Word32
@@ -65,7 +68,8 @@ data Message = Message
   , hostname :: {-# UNPACK #-} !Bytes
   , process :: !(Maybe Process)
   , message :: {-# UNPACK #-} !Bytes
-  } deriving (Show)
+  }
+  deriving (Show)
 
 data Timestamp = Timestamp
   { month :: !Chronos.Month
@@ -74,37 +78,40 @@ data Timestamp = Timestamp
   , minute :: !Word8
   , second :: !Word8
   , year :: {-# UNPACK #-} !Word32.Maybe
-    -- ^ Section 5.1 of RFC 3164 notes that some software appends
-    -- a four-character year after the time of day. Since hostnames
-    -- cannot start with digits, we can parse this unambiguously. We
-    -- extend RFC 3164 to handle these nonstandard years.
-  } deriving (Show)
+  -- ^ Section 5.1 of RFC 3164 notes that some software appends
+  -- a four-character year after the time of day. Since hostnames
+  -- cannot start with digits, we can parse this unambiguously. We
+  -- extend RFC 3164 to handle these nonstandard years.
+  }
+  deriving (Show)
 
 data Process = Process
   { priority :: {-# UNPACK #-} !Bytes
-    -- ^ Priority is nonstandard. This field is the empty byte sequence
-    -- when the priority is not present.
+  -- ^ Priority is nonstandard. This field is the empty byte sequence
+  -- when the priority is not present.
   , name :: {-# UNPACK #-} !Bytes
   , id :: {-# UNPACK #-} !Word32.Maybe
-  } deriving (Show)
+  }
+  deriving (Show)
 
 -- | Run the RFC 3164 parser. See 'parser'.
 decode :: Bytes -> Maybe Message
 decode = Parser.parseBytesMaybe parser
 
--- | Parse a RFC 3164 message. Note that this is just @takePriority@,
--- @takeTimestamp@, @takeHostname, and @takeProcess@ called in sequence,
--- followed by skipping whitespace and then treating the remaining input
--- as the original message.
+{- | Parse a RFC 3164 message. Note that this is just @takePriority@,
+@takeTimestamp@, @takeHostname, and @takeProcess@ called in sequence,
+followed by skipping whitespace and then treating the remaining input
+as the original message.
+-}
 parser :: Parser () s Message
 parser = do
   priority <- takePriority ()
   timestamp <- takeTimestamp ()
-  Latin.trySatisfy (==':') >>= \case
+  Latin.trySatisfy (== ':') >>= \case
     True -> do
       Latin.skipChar ' '
       message <- Parser.remaining
-      pure Message{priority,timestamp,hostname=Bytes.empty,process=Nothing,message}
+      pure Message {priority, timestamp, hostname = Bytes.empty, process = Nothing, message}
     False -> do
       hostname <- takeHostname ()
       -- Watchguard includes a serial number and an ISO8601-encoded datetime
@@ -113,37 +120,43 @@ parser = do
       -- an open parenthesis to appear in this position. So, by doing
       -- this, we do not reject any good logs.
       Latin.peek' () >>= \case
-        c | c >= 'A' && c <= 'Z' -> Parser.orElse
-              ( do Latin.skipWhile (\x -> (x >= 'A' && x <= 'Z') || (x >= '0' && x <= '9'))
-                   Latin.char () ' '
-                   Latin.char () '('
-                   Latin.skipDigits1 ()
-                   Latin.char () '-'
-                   Latin.skipDigits1 ()
-                   Latin.char () '-'
-                   Latin.skipDigits1 ()
-                   Latin.char () 'T'
-                   Latin.skipDigits1 ()
-                   Latin.char () ':'
-                   Latin.skipDigits1 ()
-                   Latin.char () ':'
-                   Latin.skipDigits1 ()
-                   Latin.char () ')'
-                   Latin.char () ' '
-              ) (pure ())
+        c
+          | c >= 'A' && c <= 'Z' ->
+              Parser.orElse
+                ( do
+                    Latin.skipWhile (\x -> (x >= 'A' && x <= 'Z') || (x >= '0' && x <= '9'))
+                    Latin.char () ' '
+                    Latin.char () '('
+                    Latin.skipDigits1 ()
+                    Latin.char () '-'
+                    Latin.skipDigits1 ()
+                    Latin.char () '-'
+                    Latin.skipDigits1 ()
+                    Latin.char () 'T'
+                    Latin.skipDigits1 ()
+                    Latin.char () ':'
+                    Latin.skipDigits1 ()
+                    Latin.char () ':'
+                    Latin.skipDigits1 ()
+                    Latin.char () ')'
+                    Latin.char () ' '
+                )
+                (pure ())
         _ -> pure ()
-      process <- Latin.trySatisfy (==':') >>= \case
-        True -> pure Nothing
-        False -> do
-          p <- takeProcess ()
-          pure (Just p)
+      process <-
+        Latin.trySatisfy (== ':') >>= \case
+          True -> pure Nothing
+          False -> do
+            p <- takeProcess ()
+            pure (Just p)
       Latin.skipChar ' '
       message <- Parser.remaining
-      pure Message{priority,timestamp,hostname,process,message}
+      pure Message {priority, timestamp, hostname, process, message}
 
--- | Consume the angle-bracketed priority. RFC 3164 does not allow
--- a space to follow the priority, so this does not consume a
--- trailing space.
+{- | Consume the angle-bracketed priority. RFC 3164 does not allow
+a space to follow the priority, so this does not consume a
+trailing space.
+-}
 takePriority :: e -> Parser e s Word32
 takePriority e = do
   Latin.char e '<'
@@ -151,17 +164,19 @@ takePriority e = do
   Latin.char e '>'
   pure priority
 
--- | Consume the hostname and the space that follows it. Returns
--- the hostname.
+{- | Consume the hostname and the space that follows it. Returns
+the hostname.
+-}
 takeHostname :: e -> Parser e s Bytes
 takeHostname e =
   -- TODO: This should actually use a takeWhile1.
   Latin.takeTrailedBy e ' '
 
--- | Consume the timestamp and the trailing space character if a trailing
--- space exists. Returns the parsed timestamp. This allows two extensions
--- to the RFC 3164 datetime format. The year may be present either right
--- after the day of the month or after the time of day.
+{- | Consume the timestamp and the trailing space character if a trailing
+space exists. Returns the parsed timestamp. This allows two extensions
+to the RFC 3164 datetime format. The year may be present either right
+after the day of the month or after the time of day.
+-}
 takeTimestamp :: e -> Parser e s Timestamp
 takeTimestamp e = do
   monthBytes <- Parser.take e 3
@@ -172,9 +187,10 @@ takeTimestamp e = do
   -- padded with a space.
   Latin.skipChar1 e ' '
   dayRaw <- Latin.decWord8 e
-  day <- if dayRaw < 32
-    then pure (Chronos.DayOfMonth (fromIntegral dayRaw))
-    else Parser.fail e
+  day <-
+    if dayRaw < 32
+      then pure (Chronos.DayOfMonth (fromIntegral dayRaw))
+      else Parser.fail e
   Latin.char e ' '
   hourOrYear <- Latin.decWord32 e
   Latin.any e >>= \case
@@ -191,7 +207,7 @@ takeTimestamp e = do
       second <- Latin.decWord8 e
       when (second > 59) (Parser.fail e)
       _ <- Latin.trySatisfy (== ' ')
-      pure Timestamp{month,day,hour,minute,second,year=Word32.just hourOrYear}
+      pure Timestamp {month, day, hour, minute, second, year = Word32.just hourOrYear}
     ':' -> do
       when (hourOrYear > 23) (Parser.fail e)
       let hour = fromIntegral @Word32 @Word8 hourOrYear
@@ -201,21 +217,23 @@ takeTimestamp e = do
       second <- Latin.decWord8 e
       when (second > 59) (Parser.fail e)
       Latin.trySatisfy (== ' ') >>= \case
-        False -> pure Timestamp{month,day,hour,minute,second,year=Word32.nothing}
+        False -> pure Timestamp {month, day, hour, minute, second, year = Word32.nothing}
         True -> do
           -- The only good way to allow a year is with backtracking. We do not
           -- learn until we encounter the space following the decimal number
           -- whether it was a year or part of a hostname (likely an ip address).
           Parser.orElse
-            ( do y <- Latin.decWord32 e
-                 Latin.char e ' '
-                 pure Timestamp{month,day,hour,minute,second,year=Word32.just y}
+            ( do
+                y <- Latin.decWord32 e
+                Latin.char e ' '
+                pure Timestamp {month, day, hour, minute, second, year = Word32.just y}
             )
-            (pure Timestamp{month,day,hour,minute,second,year=Word32.nothing})
+            (pure Timestamp {month, day, hour, minute, second, year = Word32.nothing})
     _ -> Parser.fail e
 
--- | Take the process name and the process id and consume the colon
--- that follows them. Does not consume any space after the colon.
+{- | Take the process name and the process id and consume the colon
+that follows them. Does not consume any space after the colon.
+-}
 takeProcess :: e -> Parser e s Process
 takeProcess e = do
   processStart <- Unsafe.cursor
@@ -224,7 +242,7 @@ takeProcess e = do
   arr <- Unsafe.expose
   let name0 = Bytes arr processStart ((processEndSucc - 1) - processStart)
       !(# name, priority #) = case Bytes.split1 0x20 name0 of
-        Just (pre,post)
+        Just (pre, post)
           | Latin1.equals3 'e' 'r' 'r' pre -> (# post, pre #)
           | Latin1.equals4 'c' 'r' 'i' 't' pre -> (# post, pre #)
           | Latin1.equals4 'i' 'n' 'f' 'o' pre -> (# post, pre #)
@@ -238,11 +256,11 @@ takeProcess e = do
           | Latin1.equals7 'w' 'a' 'r' 'n' 'i' 'n' 'g' pre -> (# post, pre #)
         _ -> (# name0, Bytes arr 0 0 #)
   case hasPid of
-    False -> pure Process{priority,name,id=Word32.nothing}
+    False -> pure Process {priority, name, id = Word32.nothing}
     True -> do
       pid <- Latin.decWord32 e
       Latin.char2 e ']' ':'
-      pure Process{priority,name,id=Word32.just pid}
+      pure Process {priority, name, id = Word32.just pid}
 
 -- Precondition: length of bytes is 3
 resolveMonth :: Bytes -> Chronos.Month
--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,11 @@
-{-# language DuplicateRecordFields #-}
-{-# language NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 import Prelude hiding (id)
 
 import Data.Bytes (Bytes)
 import Data.Maybe (isNothing)
-import Syslog.Bsd (Message(Message),Process(Process))
+import Syslog.Bsd (Message (Message), Process (Process))
 
 import qualified Data.Bytes as Bytes
 import qualified Data.Bytes.Text.Latin1 as Latin1
@@ -19,7 +19,7 @@ main = do
   putStrLn "Test A"
   case Bsd.decode msgA of
     Nothing -> fail "Could not decode message A"
-    Just Message{priority,hostname,process=Just Process{name},message} -> do
+    Just Message {priority, hostname, process = Just Process {name}, message} -> do
       assert "priority" (priority == 133)
       assert "hostname" (hostname == Latin1.fromString "webserver")
       assert "process_name" (name == Latin1.fromString "syslogd")
@@ -28,7 +28,7 @@ main = do
   putStrLn "Test B"
   case Bsd.decode msgB of
     Nothing -> fail "Could not decode message B"
-    Just Message{priority,hostname,process=Just Process{name},message} -> do
+    Just Message {priority, hostname, process = Just Process {name}, message} -> do
       assert "priority" (priority == 0)
       assert "hostname" (hostname == Latin1.fromString "foo.example.org")
       assert "process_name" (name == Latin1.fromString "sched")
@@ -37,7 +37,7 @@ main = do
   putStrLn "Test C"
   case Bsd.decode msgC of
     Nothing -> fail "Could not decode message C"
-    Just Message{priority,hostname,process=Just Process{name},message} -> do
+    Just Message {priority, hostname, process = Just Process {name}, message} -> do
       assert "priority" (priority == 133)
       assert "hostname" (hostname == Latin1.fromString "192.0.2.231")
       assert "process_name" (name == Latin1.fromString "stm")
@@ -46,50 +46,50 @@ main = do
   putStrLn "Test D"
   case Bsd.decode msgD of
     Nothing -> fail "Could not decode message D"
-    Just Message{priority,process,message} -> do
+    Just Message {priority, process, message} -> do
       assert "priority" (priority == 26)
       assert "process_name" (isNothing process)
       assert "message" (message == Latin1.fromString "ASA log")
   putStrLn "Test E"
   case Bsd.decode msgE of
     Nothing -> fail "Could not decode message E"
-    Just Message{process} -> case process of
-      Just Process{priority,name} -> do
+    Just Message {process} -> case process of
+      Just Process {priority, name} -> do
         assert "process_priority" (priority == Latin1.fromString "notice")
         assert "process_name" (name == Latin1.fromString "tmsh")
       Nothing -> fail "Message E missing process information"
   putStrLn "Test F"
   case Bsd.decode msgF of
     Nothing -> fail "Could not decode message F"
-    Just Message{message} -> do
+    Just Message {message} -> do
       assert "message" (message == Latin1.fromString "foo")
   putStrLn "Test G"
   case Bsd.decode msgG of
     Nothing -> fail "Could not decode message G"
-    Just{} -> pure ()
+    Just {} -> pure ()
   putStrLn "Test IETF A"
   case Ietf.decode ietfA of
     Nothing -> fail "Could not decode IETF message A"
-    Just Ietf.Message{version,hostname,structuredData} -> do
+    Just Ietf.Message {version, hostname, structuredData} -> do
       assert "version" (version == 1)
       assert "hostname" (hostname == Latin1.fromString "mymachine.example.com")
       assert "structured_data_length" (length structuredData == 1)
   putStrLn "Test IETF B"
   case Ietf.decode ietfB of
     Nothing -> fail "Could not decode IETF message B"
-    Just Ietf.Message{version,hostname,application,messageType,structuredData} -> do
+    Just Ietf.Message {version, hostname, application, messageType, structuredData} -> do
       assert "version" (version == 1)
       assert "hostname" (hostname == Latin1.fromString "FOOBAR-SRX-FWL0")
       assert "application" (application == Latin1.fromString "RT_FLOW")
       assert "message_type" (messageType == Latin1.fromString "RT_FLOW_SESSION_CLOSE")
       assert "structured_data_length" (length structuredData == 1)
-      let Ietf.Element{id,parameters} = PM.indexSmallArray structuredData 0
+      let Ietf.Element {id, parameters} = PM.indexSmallArray structuredData 0
       assert "structured_data.id" (id == Latin1.fromString "junos@2636.1.1.1.2.133")
       assert "structured_data.parameters_length" (length parameters == 32)
   putStrLn "Test IETF C"
   case Ietf.decode ietfC of
     Nothing -> fail "Could not decode IETF message C"
-    Just Ietf.Message{version,hostname,application,messageType,structuredData,message} -> do
+    Just Ietf.Message {version, hostname, application, messageType, structuredData, message} -> do
       assert "version" (version == 1)
       assert "hostname" (hostname == Latin1.fromString "mymachine.example.com")
       assert "application" (application == Latin1.fromString "bigapp")
@@ -99,11 +99,11 @@ main = do
   putStrLn "Test IETF D"
   case Ietf.decode ietfD of
     Nothing -> fail "Could not decode IETF message D"
-    Just Ietf.Message{message,structuredData} -> do
+    Just Ietf.Message {message, structuredData} -> do
       assert "structured_data_length" (length structuredData == 3)
-      let Ietf.Element{parameters} = PM.indexSmallArray structuredData 2
+      let Ietf.Element {parameters} = PM.indexSmallArray structuredData 2
       assert "parameters_length" (length parameters == 1)
-      let Ietf.Parameter{value} = PM.indexSmallArray parameters 0
+      let Ietf.Parameter {value} = PM.indexSmallArray parameters 0
       case value == Latin1.fromString "\\foo\\bar.txt" of
         True -> pure ()
         False -> fail ("structured_data.2.0: " ++ show value)
@@ -131,52 +131,62 @@ msgF = Latin1.fromString "<133>Aug 10 07:12:13: foo"
 msgG = Latin1.fromString "<134>Oct 30 12:31:49 10.22.16.11 logrhythm: <Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'>"
 
 ietfA :: Bytes
-ietfA = Latin1.fromString $ concat
-  [ "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 "
-  , "[exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] "
-  , "BOMAn application event log entry"
-  ]
+ietfA =
+  Latin1.fromString $
+    concat
+      [ "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 "
+      , "[exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] "
+      , "BOMAn application event log entry"
+      ]
 
 ietfB :: Bytes
-ietfB = Latin1.fromString $ concat
-  [ "<14>1 2020-10-15T17:01:23.466Z FOOBAR-SRX-FWL0 RT_FLOW - RT_FLOW_SESSION_CLOSE "
-  , "[junos@2636.1.1.1.2.133 reason=\"application failure or action\" "
-  , "source-address=\"192.0.2.29\" source-port=\"55110\" "
-  , "destination-address=\"192.0.2.30\" destination-port=\"135\" connection-tag=\"0\" "
-  , "service-name=\"junos-ms-rpc-tcp\" nat-source-address=\"192.0.2.229\" "
-  , "nat-source-port=\"55110\" nat-destination-address=\"192.0.2.230\" "
-  , "nat-destination-port=\"135\" nat-connection-tag=\"0\" src-nat-rule-type=\"N/A\" "
-  , "src-nat-rule-name=\"N/A\" dst-nat-rule-type=\"N/A\" dst-nat-rule-name=\"N/A\" "
-  , "protocol-id=\"6\" policy-name=\"EXAMPLE-POLICY\" "
-  , "source-zone-name=\"MYSRCZONE\" destination-zone-name=\"MYDSTZONE\" "
-  , "session-id-32=\"14953\" packets-from-client=\"0\" bytes-from-client=\"0\" "
-  , "packets-from-server=\"0\" bytes-from-server=\"0\" elapsed-time=\"1\" "
-  , "application=\"UNKNOWN\" nested-application=\"UNKNOWN\" username=\"N/A\" "
-  , "roles=\"N/A\" packet-incoming-interface=\"ge-0/0/5.0\" encrypted=\"UNKNOWN\"] "
-  , "session closed application failure or action"
-  ]
+ietfB =
+  Latin1.fromString $
+    concat
+      [ "<14>1 2020-10-15T17:01:23.466Z FOOBAR-SRX-FWL0 RT_FLOW - RT_FLOW_SESSION_CLOSE "
+      , "[junos@2636.1.1.1.2.133 reason=\"application failure or action\" "
+      , "source-address=\"192.0.2.29\" source-port=\"55110\" "
+      , "destination-address=\"192.0.2.30\" destination-port=\"135\" connection-tag=\"0\" "
+      , "service-name=\"junos-ms-rpc-tcp\" nat-source-address=\"192.0.2.229\" "
+      , "nat-source-port=\"55110\" nat-destination-address=\"192.0.2.230\" "
+      , "nat-destination-port=\"135\" nat-connection-tag=\"0\" src-nat-rule-type=\"N/A\" "
+      , "src-nat-rule-name=\"N/A\" dst-nat-rule-type=\"N/A\" dst-nat-rule-name=\"N/A\" "
+      , "protocol-id=\"6\" policy-name=\"EXAMPLE-POLICY\" "
+      , "source-zone-name=\"MYSRCZONE\" destination-zone-name=\"MYDSTZONE\" "
+      , "session-id-32=\"14953\" packets-from-client=\"0\" bytes-from-client=\"0\" "
+      , "packets-from-server=\"0\" bytes-from-server=\"0\" elapsed-time=\"1\" "
+      , "application=\"UNKNOWN\" nested-application=\"UNKNOWN\" username=\"N/A\" "
+      , "roles=\"N/A\" packet-incoming-interface=\"ge-0/0/5.0\" encrypted=\"UNKNOWN\"] "
+      , "session closed application failure or action"
+      ]
 
 ietfC :: Bytes
-ietfC = Latin1.fromString $ concat
-  [ "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com bigapp - - - "
-  , "hey world"
-  ]
+ietfC =
+  Latin1.fromString $
+    concat
+      [ "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com bigapp - - - "
+      , "hey world"
+      ]
 
 ietfD :: Bytes
-ietfD = Latin1.fromString $ concat
-  [ "<38>1 2021-11-18T11:55:55.661764Z 192.0.2.20 SentinelOne "
-  , "ab1fc131b2f29bc49b09286bb05e0b94e5c36610 1291980691205274618 "
-  , "[fileName@53163 fileName=\"badcat.exe\"]"
-  , "[deviceAddress@53163 deviceAddress=\"192.0.2.21\"]"
-  , "[path@53163 path=\"\\\\foo\\\\bar.txt\"]"
-  , " bad news"
-  ]
+ietfD =
+  Latin1.fromString $
+    concat
+      [ "<38>1 2021-11-18T11:55:55.661764Z 192.0.2.20 SentinelOne "
+      , "ab1fc131b2f29bc49b09286bb05e0b94e5c36610 1291980691205274618 "
+      , "[fileName@53163 fileName=\"badcat.exe\"]"
+      , "[deviceAddress@53163 deviceAddress=\"192.0.2.21\"]"
+      , "[path@53163 path=\"\\\\foo\\\\bar.txt\"]"
+      , " bad news"
+      ]
 
 ietfE :: Bytes
-ietfE = Latin1.fromString $ concat
-  [ "<14>1 2021-12-10T09:19:19.614-05:00 THE-HOST RT_FLOW - RT_FLOW_SESSION_DENY "
-  , "[junos@2636.1.1.1.2.40 source-address=\"192.0.2.13\"]"
-  ]
+ietfE =
+  Latin1.fromString $
+    concat
+      [ "<14>1 2021-12-10T09:19:19.614-05:00 THE-HOST RT_FLOW - RT_FLOW_SESSION_DENY "
+      , "[junos@2636.1.1.1.2.40 source-address=\"192.0.2.13\"]"
+      ]
 
 ietfF :: Bytes
 ietfF = Latin1.fromString "<14>1 2023-05-09T16:21:33-04:00 192.0.2.5 - - - -  CEF:0"
